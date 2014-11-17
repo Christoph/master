50c360cdfaf43b860b8ecbc2f73924595068201c
Chris <christoph.kralj@gmail.com>
2014-11-17 19:01:46 +0100

    Simple checks if tag/track/artist exist and import if not.

--- src/tags/DB.java
+++ src/tags/DB.java
@@ -9,32 +9,72 @@ import org.apache.commons.dbutils.*;
 import de.umass.lastfm.*;
 
 public class DB {
-  private String connectionString;
-  private String user;
-  private String pass;
+  private String connectionString user, pass;
+  private String tagQuery, artistQuery, trackQuery;
+  private int trackID, tagID;
   private Connection conn;
-  private String query;
+
   private QueryRunner run;
-  
+  private Query querymanager;
+
   public DB(Properties config) {
     connectionString = config.getProperty("database");
-    connectionString = config.getProperty("user");
-    connectionString = config.getProperty("password");
+    user = config.getProperty("user");
+    pass = config.getProperty("password");
+
+    querymanager = new Query();
   }
 
   public void insert(Collection<Tag> tags) {
     //Initialization
     run = new QueryRunner();
   
+    // Checks
     for(Tag t: tags)
     {
-      // Check
-      // TO BE DONE
+      // Check if the artist exists
+      if("SELECT Name FROM Artist WHERE Name = "+t.getArtist() == t.getArtist());
+      {
+        artistQuery = "";
+      }
+      else
+      {
+        artistQuery = "INSERT INTO Artist VALUES(DEFAULT,'"+t.getArtist()+"');";
+      }
       
-      // Build query
-      query = "INSERT INTO Tag VALUES(DEFAULT,'"+t.getName()+"');";
+      // Check if the track exists
+      if("SELECT Name FROM Track WHERE Name = "+t.getTrack() == t.getTrack());
+      {
+        trackID = "SELECT ID FROM Track WHERE Name = "+t.getTrack()";
+        trackQuery = "";
+      }
+      else
+      {
+        trackQuery = "INSERT INTO Track VALUES(DEFAULT,'"+t.getTrack()+"',"+"SELECT ID FROM Artist WHERE Name = "+t.getArtist();+");";
+      }
+
+      // Check if the tag exists
+      if("SELECT Name FROM Tag WHERE Name = "+t.getName() == t.getName());
+      {
+        tagID = "SELECT ID FROM Tag WHERE Name = "+t.getTrack()";
+        tagQuery = "";
+      }
+      else
+      {
+        tagQuery = "INSERT INTO Tag VALUES(DEFAULT,'"+t.getName()+"');";
+      }
       
-      // Execute the query
+      // Check if the tag/track combination exists
+      if("SELECT * FROM TT WHERE TrackID = "+trackID.toString()+" AND TagID = "+tagID.toString() == null)
+      {
+        ttQuery = "";
+      }
+      else
+      {
+        ttQuery = "INSERT INTO TT VALUES(DEFAULT,"+trackID+","tagID");";
+      }
+
+      // Execute the querys
       try {     
         conn = DriverManager.getConnection(connectionString, user, pass);
         run.update(conn, query);
