06562822a3317f70561408e73958e73980ce4bc1
Chris <christoph.kralj@gmail.com>
2014-11-17 16:15:07 +0100

    Created database config file.

--- src/tags/Core.java
+++ src/tags/Core.java
@@ -1,21 +1,42 @@
 package tags;
 
 import java.util.Collection;
+
 import de.umass.lastfm.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Core {
 
-  private static Collection<Tag> tags;
 
   public static void main(String[] args) {
-	DB db = new DB();
-	LastFM last = new LastFM();
-		
-	System.out.println("Start");
-	
-	tags = last.mineTags("Metallica", "Nothing Else Matters");
-    db.insert(tags);
+    // Initializing classes and variables
+    Collection<Tag> tags;
+
+    Properties dbconf = new Properties();
+    InputStream input = null;
+
+    // Load config files
+    try {
+
+      input = new FileInputStream("config.db");
+      dbconf.load(input);
+
+    } catch (IOException e) { e.printStackTrace(); }
+
+    DB db = new DB(dbconf);
+    LastFM last = new LastFM();
+      
+    // Program
+    System.out.println("Start");
+
     
+    tags = last.mineTags("Metallica", "Nothing Else Matters");
+    db.insert(tags);
+      
     System.out.println("End");
   }
 }
--- src/tags/DB.java
+++ src/tags/DB.java
@@ -2,40 +2,47 @@ package tags;
 
 import java.sql.*;
 import java.util.Collection;
+import java.util.Properties;
 
 import org.apache.commons.dbutils.*;
 
 import de.umass.lastfm.*;
 
 public class DB {
-	private String connectionString = "jdbc:mariadb://localhost/tags";
-	private String user = "root";
-	private String pass = "sh0wt1me";
-	private Connection conn;
-	private String query;
-	private QueryRunner run;
-	
-	public void insert(Collection<Tag> tags) {
-		//Initialization
-		 run = new QueryRunner();
-	
-		for(Tag t: tags)
-		{
-			// Check
-			// TO BE DONE
-			
-			// Build query
-			query = "INSERT INTO Tag VALUES(DEFAULT,'"+t.getName()+"');";
-			
-			// Execute the query
-			try {   	
-				conn = DriverManager.getConnection(connectionString, user, pass);
-				run.update(conn, query);
-			} catch(SQLException e) {
-				e.printStackTrace();
-			} finally {
-				DbUtils.closeQuietly(conn);
-			}
-		}
-	}
+  private String connectionString;
+  private String user;
+  private String pass;
+  private Connection conn;
+  private String query;
+  private QueryRunner run;
+  
+  public DB(Properties config) {
+    connectionString = config.getProperty("database");
+    connectionString = config.getProperty("user");
+    connectionString = config.getProperty("password");
+  }
+
+  public void insert(Collection<Tag> tags) {
+    //Initialization
+    run = new QueryRunner();
+  
+    for(Tag t: tags)
+    {
+      // Check
+      // TO BE DONE
+      
+      // Build query
+      query = "INSERT INTO Tag VALUES(DEFAULT,'"+t.getName()+"');";
+      
+      // Execute the query
+      try {     
+        conn = DriverManager.getConnection(connectionString, user, pass);
+        run.update(conn, query);
+      } catch(SQLException e) {
+        e.printStackTrace();
+      } finally {
+        DbUtils.closeQuietly(conn);
+      }
+    }
+  }
 }
