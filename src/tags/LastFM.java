4295ebf541c0290e7f60e1547b2d4a80a1745fbb
Chris <christoph.kralj@gmail.com>
2014-11-27 12:59:57 +0100

    Rearanged parameters.

--- src/tags/LastFM.java
+++ src/tags/LastFM.java
@@ -8,7 +8,7 @@ public class LastFM {
 	private String key = "97d879c2f3a01fbbfb007be0eb86734e"; 
 
   //Get the tags from last.fm
-  public Collection<Tag> mineTags(String artist, String title) {
+  public Collection<Tag> mineTags(String title, String artist) {
     return Track.getTopTags(artist, title, key);    
   }
 }
