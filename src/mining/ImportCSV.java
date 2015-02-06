b4742a38be3f54137274a824ae62e901d2383f3e
Chris <christoph.kralj@gmail.com>
2015-02-06 12:36:53 +0100

    Changed encoding to UTF-8.

--- src/mining/ImportCSV.java
+++ src/mining/ImportCSV.java
@@ -2,7 +2,7 @@ package mining;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.nio.charset.*;
 import java.nio.file.Files;
 import java.util.*;
 
@@ -13,7 +13,7 @@ public class ImportCSV {
     List<String> lines = null;
     try {
       File file = new File("db/complete.csv");
-      lines = Files.readAllLines(file.toPath(), Charset.defaultCharset());
+      lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
     } catch (IOException e) { e.printStackTrace(); }
 
     return lines;
