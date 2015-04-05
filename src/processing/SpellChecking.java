298e20ba88c0d4e738329566a0148c11ec37d21c
Chris <christoph.kralj@gmail.com>
2015-04-05 21:03:21 +0200

    Added custom file for custom blacklist words and fnished the spellchecking algorithm.

---
+++ src/processing/SpellChecking.java
@@ -0,0 +1,182 @@
+package processing;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.codec.language.DoubleMetaphone;
+
+import tags.Tag;
+import tags.TagsToCSV;
+
+public class SpellChecking {
+	
+	public void check(List<Tag> tags, List<String> blacklist)
+	{
+	    /////////////////////////////////
+	    // Variables
+	    PlainStringSimilarity psim = new PlainStringSimilarity();
+		TagsToCSV writer_subs = new TagsToCSV("subs.csv");
+		
+	    Map<String, Integer> wordgrams = new HashMap<String, Integer>();
+	    Map<String, Set<String>> phon = new HashMap<String, Set<String>>();
+	    Map<String, String> subs = new HashMap<String, String>();
+	    
+	    List<String> words;
+	    int value, count, ngram_size;
+	    double dist;
+	    String key, p, new_tag;
+	    Boolean export_substitutions;
+        HashSet<String> h1, h2;
+	    
+		/////////////////////////////////
+		// Configuration
+		
+		// Choose phonetic algorithm
+		DoubleMetaphone phonetic = new DoubleMetaphone();
+		//ColognePhonetic phonetic = new ColognePhonetic();
+		
+		// Set output of substitution list
+		export_substitutions = false;
+		
+		// Set the size for the n-gram distance method
+		ngram_size = 2;
+		
+		// Choose distance metric on line 143
+		
+		/////////////////////////////////
+		// Algorithm		
+	    
+	    // Create 1-word-gram/total count dict
+	    for(int i = 0;i < tags.size(); i++)
+	    {
+	    	words = psim.create_word_gram(tags.get(i).getTagName(),blacklist);
+	    	
+	    	for(int j = 0; j < words.size(); j++)
+	    	{
+	    		key = words.get(j); 		
+
+	    		if(wordgrams.containsKey(key))
+	    		{
+	    			value = wordgrams.get(key);
+	    			
+	    			// Sum up the count
+	    			wordgrams.put(key, value + tags.get(i).getListeners());
+	    		}
+	    		else
+	    		{
+	    			wordgrams.put(key, tags.get(i).getListeners());
+	    		}
+	    	}
+	    }
+	    
+	    // Create phonetic dictionary
+	    for(String k: wordgrams.keySet())
+	    {
+	    	
+	    	//p = dmeta.encode(k);
+	    	
+	    	p = phonetic.encode(k);
+	    	
+	    	if(phon.containsKey(p))
+	    	{
+	    		Set<String> temp;
+	    		
+	    		temp = phon.get(p);
+	    		temp.add(k);
+	    		
+	    		phon.put(p, temp);
+	    		
+	    	}
+	    	else
+	    	{
+	    		Set<String> temp = new HashSet<String>();
+	    		
+	    		temp.add(k);
+	    		
+	    		phon.put(p, temp);
+	    	}
+	    }
+	    
+	    // Create word set/count dict  
+	    for(int i = 0;i < tags.size(); i++)
+	    {
+	    	words = psim.create_word_gram(tags.get(i).getTagName(),blacklist);
+	    	
+	    	for(int j = 0; j < words.size(); j++)
+	    	{
+	    		String tag = words.get(j);
+
+	        if(!subs.containsKey(tag))
+	        {
+	          String code = phonetic.encode(tag);
+	          String high = "";
+	          
+	          Set<String> phonetics = phon.get(code);
+	          
+	          while(!phonetics.isEmpty())
+	          {
+	            count = 0;
+
+	            for(String s: phonetics)
+	            {
+	              if(wordgrams.get(s) > count)
+	              {
+	                high = s;
+	                count = wordgrams.get(s);
+	              }
+	            }
+	            
+	            for(Iterator<String> iterator = phonetics.iterator(); iterator.hasNext();)
+	            {
+	              String word = iterator.next();
+	              dist = 0;
+	              
+	              h1 = psim.create_n_gram(word, ngram_size);
+	              h2 = psim.create_n_gram(high, ngram_size);
+	              
+	              // Choose distance methods
+	              dist = psim.dice_coeffizient(h1, h2);
+	              //dist = psim.jaccard_index(h1, h2);
+	              //dist = psim.cosine_similarity(h1, h2);
+	              
+	              if(dist > 0.7)
+	              {
+	                subs.put(word, high);
+	                iterator.remove();
+	              }  
+	            }
+	          }
+	        }   
+	      }
+	    }
+
+	    // Export substitution list
+	    if(export_substitutions)
+	    {
+	    	writer_subs.writeSubs(subs);
+	    }
+	    
+	    // Replace tags corresponding to the subs dict
+	    for(Tag t: tags)
+	    {
+	      words = psim.create_word_gram(t.getTagName(),blacklist);
+	      new_tag = "";
+	    	
+	      for(String w: words)
+	      {
+	        new_tag = new_tag + " " + subs.get(w);
+	      }
+
+	      t.setTagName(new_tag);
+	    }
+	    
+	    wordgrams = null;
+	    phon = null;
+	    subs = null;
+	}
+	
+}
--- src/tags/Core.java
+++ src/tags/Core.java
@@ -79,25 +79,22 @@ public class Core {
     ImportCSV im = new ImportCSV();
     PlainStringSimilarity psim = new PlainStringSimilarity();
     TagsToCSV writer = new TagsToCSV("tags.csv");
-    TagsToCSV writer_subs = new TagsToCSV("subs.csv");
-    
-    DoubleMetaphone phonetic = new DoubleMetaphone();
-    //ColognePhonetic phonetic = new ColognePhonetic();
+    SpellChecking checker = new SpellChecking();
     
     //List<String> genres = im.importCSV("dicts/genres.txt");
     List<String> articles = im.importCSV("dicts/article.txt");
     //List<String> moods = im.importCSV("dicts/moods.txt");
     List<String> preps = im.importCSV("dicts/prep.txt");   
+    List<String> custom = im.importCSV("dicts/custom.txt"); 
     
     // Create word blacklist
     List<String> blacklist = new ArrayList<String>();
     
     blacklist.addAll(preps);
     blacklist.addAll(articles);
+    blacklist.addAll(custom);
+    // Somehow some tags have words without characters...
     blacklist.add("");
-    blacklist.add("and");
-    blacklist.add("I");
-    blacklist.add("am");
     
     /////////////////////////////////
     // Spell checking
@@ -108,149 +105,16 @@ public class Core {
     
     //pro.exportAll("tags.csv");
     
-    
-    // Firsts run => Spelling
-    // Create the n-grams and put them into a dictionary
-    Map<String, Integer> ngrams = new HashMap<String, Integer>();
-    List<String> words;
-    int value;
-    String key;
-    
-    // Create 1-word-gram/total count dict
-    for(int i = 0;i < tags.size(); i++)
-    {
-    	words = psim.create_word_gram(tags.get(i).getTagName(),blacklist);
-    	
-    	for(int j = 0; j < words.size(); j++)
-    	{
-    		key = words.get(j); 		
-
-    		if(ngrams.containsKey(key))
-    		{
-    			value = ngrams.get(key);
-    			
-    			// Sum up the count
-    			ngrams.put(key, value + tags.get(i).getListeners());
-    		}
-    		else
-    		{
-    			ngrams.put(key, tags.get(i).getListeners());
-    		}
-    	}
-    }
-    
-    // Create phonetic dictionary
-    Map<String, Set<String>> phon = new HashMap<String, Set<String>>();
-    String p;
-    
-    for(String k: ngrams.keySet())
-    {
-    	
-    	//p = dmeta.encode(k);
-    	
-    	p = phonetic.encode(k);
-    	
-    	if(phon.containsKey(p))
-    	{
-    		Set<String> temp;
-    		
-    		temp = phon.get(p);
-    		temp.add(k);
-    		
-    		phon.put(p, temp);
-    		
-    	}
-    	else
-    	{
-    		Set<String> temp = new HashSet<String>();
-    		
-    		temp.add(k);
-    		
-    		phon.put(p, temp);
-    	}
-    }
-    
-    // Subsitution dict
-    Map<String, String> subs = new HashMap<String, String>();
-    
-    // Create word set/count dict  
-    for(int i = 0;i < tags.size(); i++)
-    {
-    	words = psim.create_word_gram(tags.get(i).getTagName(),blacklist);
-      int count;
-    	
-    	for(int j = 0; j < words.size(); j++)
-    	{
-    		String tag = words.get(j);
-
-        if(!subs.containsKey(tag))
-        {
-          String code = phonetic.encode(tag);
-          String high = "";
-          
-          Set<String> phonetics = phon.get(code);
-          
-          while(!phonetics.isEmpty())
-          {
-            count = 0;
-
-            for(String s: phonetics)
-            {
-              if(ngrams.get(s) > count)
-              {
-                high = s;
-                count = ngrams.get(s);
-              }
-            }
-            
-            for(Iterator<String> iterator = phonetics.iterator(); iterator.hasNext();)
-            {
-              String word = iterator.next();
-              HashSet<String> h1, h2;
-              double dist = 0;
-              
-              h1 = psim.create_n_gram(word, 2);
-              h2 = psim.create_n_gram(high, 2);
-              
-              dist = psim.dice_coeffizient(h1, h2);
-              //dist = psim.jaccard_index(h1, h2);
-              //dist = psim.cosine_similarity(h1, h2);
-              
-              if(dist > 0.7)
-              {
-                subs.put(word, high);
-                iterator.remove();
-              }  
-            }
-          }
-        }   
-      }
-    }
-
-    writer_subs.writeSubs(subs);
-    
-    // Replace tags corresponding to the subs dict
-    
-    for(Tag t: tags)
-    {
-    	words = psim.create_word_gram(t.getTagName(),blacklist);
-      String new_tag = "";
-    	
-      for(String w: words)
-    	{
-        new_tag = new_tag + " " + subs.get(w);
-      }
-
-      t.setTagName(new_tag);
-    }
+    // Basic spell checking
+    checker.check(tags, blacklist);
 
     writer.writeTagNames(tags);
     
-    // Second run => Synonyms 
-
+    // Removing rare and less used tags
+    
     
     // Decision with Torsten: Removing all tracks with less than six tags.
-		// pro.deleteTracksWithTagsLessThan(6);
+	// pro.deleteTracksWithTagsLessThan(6);
     
     // Close all
     pro.closeAll();
