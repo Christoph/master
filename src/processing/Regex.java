76212cd8255e4e74ad76fc9c495f30ec5ed2a8fb
Chris <christoph.kralj@gmail.com>
2015-08-11 18:58:15 +0200

    Added regex word separator.

--- src/processing/Regex.java
+++ src/processing/Regex.java
@@ -3,7 +3,6 @@ package processing;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,64 +11,104 @@ import tags.TagsToCSV;
 
 public class Regex {
 
-	public void separateWords(List<Tag> tags, Set<String> list)
-	{
-	  	String name = "";
-	  	String join = "";
-	  	TagsToCSV writer;
-	  	List<String> separation = new ArrayList<String>();
-	  	
-	  	// Debug output
-	  	Boolean print_groups = true;
-	  	
-	    // Create a Pattern object
-	    Pattern r;
+	String name = "";
+  	String join = "";
 
-	    // Now create matcher object.
-	    Matcher m;
-		
+	String ls = "";
+	String ms = "";
+	String rs = "";
+  	
+  	TagsToCSV writer;
+  	List<String> separation = new ArrayList<String>();
+  	public List<String> out = new ArrayList<String>();
+  	
+  	// Debug output
+  	Boolean print_groups = true;
+  	
+    // Create a Pattern object
+    Pattern r, l;
+
+    // Now create matcher object.
+    Matcher mr, ml;
+	
+    // String, > 0 == right, list of important tags
+	public void matcher(String name, List<String> list)
+	{
+			for(String s: list)
+			{
+  			// Compile patterns
+			l = Pattern.compile("(.*)("+s.toLowerCase()+")(.*)");  
+			r = Pattern.compile("(.*)("+s.toLowerCase()+")(.*)");  
+			
+	  		// Find matches
+	  		mr = r.matcher(name);
+	  		ml = l.matcher(name);
+	  		
+	  		// Check if a match happened
+	  		if(mr.find())
+	  		{
+	  			String ls = mr.group(1).trim();
+	  			String ms = mr.group(2).trim();
+	  			String rs = mr.group(3).trim();
+	  			
+	  			if(ls.length() > 0) matcher(ls, list);
+	  			
+	  			out.add(ms);
+	  			
+	  			if(rs.length() > 0) matcher(rs, list);
+	  			
+	  			name = "";
+	  		} 			  		
+	  		else if(ml.find())
+	  		{
+	  			String ls = ml.group(1).trim();
+	  			String ms = ml.group(2).trim();
+	  			String rs = ml.group(3).trim();
+	  			
+	  			if(ls.length() > 0) matcher(ls, list);
+
+	  			out.add(ms);
+	  			
+	  			if(rs.length() > 0) matcher(rs, list);
+	  			
+	  			name = "";
+	  		}
+  		}
+			
+
+		out.add(name.trim());
+
+	}
+	
+	public void separateWords(List<Tag> tags, List<String> list)
+	{
 		for(Tag t: tags)
 		{
 			// Set tag name
 			name = t.getTagName();
-
-			// Reset join string
+			
+			// Reset join string and out list
 	  		join = "";
+	  		out.clear();
 	  		
-	  		for(String s: list)
+	  		// Apply regex
+	  		matcher(name, list);
+	  		
+	  		// Rebuild string from out
+	  		for(String s: out)
 	  		{
-	  			r = Pattern.compile("(.*\\w+)("+s.toLowerCase()+")(.*)|(.*)("+s.toLowerCase()+")(\\w+.*)");  
-	  			
-		  		// Find matches
-		  		m = r.matcher(name);
-		  		
-		  		if(m.find())
-		  		{	
-		  			for(int i = 1;i<7;i++)
-		  			{
-		  				if(m.group(i)!=null)
-		  				{
-		  	  				join = join + " " + m.group(i);	
-		  				}
-		  			}
-		  			
-		  			
-		  			separation.add(s+":"+name+"->"+join);
-		  			
-					//System.out.println("");
-					//System.out.println("Song:"+t.getSongName());
-					//System.out.println("Old:"+name);
-		  			//System.out.println("Match:"+s);
-		  			//System.out.println("New:"+join);
-
-		  			name = join;
-		  			
-		  			join = "";
-		  			m.reset();
-		  		}
+	  			if(s.length() > 0)
+	  			{
+	  				join = join.concat(" "+s);
+	  			}
 	  		}
 	  		
-	  		t.setTagName(name);
+	  		join = join.trim();
+	  		name = name.trim();
+	  		
+	  		t.setTagName(join);
+	  		
+	  		if(!name.equals(join)) separation.add(name+" -> "+join);
 		}
 		
 		// Write temp files
