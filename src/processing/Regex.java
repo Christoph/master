325bb7800b0187d36ec40d88423ee696b0935a83
Chris <christoph.kralj@gmail.com>
2015-08-23 16:54:19 +0200

    Increased minWordLength by 1 for the regex matcher.

--- src/processing/Regex.java
+++ src/processing/Regex.java
@@ -51,7 +51,7 @@ public class Regex {
   			// Compile patterns
 			// If bigger than min word length do substring search
 			// else only full word search
-			if(e.length() > minWordLength)
+			if(e.length() > minWordLength+1)
 			{
 				l = Pattern.compile("(.*)("+e+")(.*)");  
 				r = Pattern.compile("(.*)("+e+")(.*)");  
