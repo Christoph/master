eebf0446809ff87bf84b48bc8716190e3bc815c5
Chris <christoph.kralj@gmail.com>
2016-03-05 00:23:40 +0100

    Added english stopwords to blacklist for default.

--- src/server/Workflow.java
+++ src/server/Workflow.java
@@ -102,11 +102,6 @@ public class Workflow {
 		
 		// Compute word frequency
 		help.wordFrequency(tags.get(0), tagsFreq);
-		
-		client.sendEvent("preFilterData", sendPreFilterHistogram());
-		client.sendEvent("preFilterGrid", sendPreFilter());
-
-		running = true;
 	}
 	
 	public void applyImportedDataCount(int count) {
@@ -129,6 +124,9 @@ public class Workflow {
 		{
 			computePreprocessing(client);
 		}
+
+		running = true;
+		client.sendEvent("isRunning",sendStatus());
 	}
 
 	private void applyDefaults() {
@@ -140,6 +138,10 @@ public class Workflow {
 		preprocess.addReplace("/, ");
 
 		preprocess.setRemove("'");
+
+		String stopwords = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your";
+
+		Collections.addAll(blacklist, stopwords.split(","));
 
 		// Spell correct
 		spellcorrect.setSpellImportance(0.7);
@@ -159,6 +161,10 @@ public class Workflow {
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	public void computePreprocessing(SocketIOClient client) {
+		// Send pre filter data
+		client.sendEvent("preFilterData", sendPreFilterHistogram());
+		client.sendEvent("preFilterGrid", sendPreFilter());
+
 		help.resetStep(tags, 1);
 
 		// Remove tags
